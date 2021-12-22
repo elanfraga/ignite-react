e119f71cd19fdbbce2ba000286a7d4d69c891aa3
Elan Fraga <elanfraga@gmail.com>
2021-12-22 13:37:05 -0300

    Salvando dados do form

--- 02-dtmoney/src/styles/global.ts
+++ 02-dtmoney/src/styles/global.ts
@@ -32,24 +32,21 @@ export const GlobalStyle = createGlobalStyle`
     }
   }
 
-  body {
+  ody {
     background: var(--background);
     -webkit-font-smoothing: antialiased;
   }
-
-  border-style, input, textarea, button {
+  body, input, textarea, button {
     font-family: 'Poppins', sans-serif;
     font-weight: 400;
   }
-
-  h1, h2, h3, h4, h5, h6, strong {
+  
+  h1,h2,h3,h4,h5, h5 {
     font-weight: 600;
   }
-
   button {
     cursor: pointer;
   }
-
   [disabled] {
     opacity: 0.6;
     cursor: not-allowed;
