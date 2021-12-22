aae6bd8582ef16ba50b564cd9235a25aebf2c010
Elan Fraga <elanfraga@gmail.com>
2021-12-22 14:28:48 -0300

    Formatando valores

--- 02-dtmoney/src/components/TransactionsTable/index.tsx
+++ 02-dtmoney/src/components/TransactionsTable/index.tsx
@@ -8,7 +8,7 @@ interface Transaction {
   amount: number;
   type: string;
   category: string;
-  createAt: string;
+  createdAt: string;
 }
 export function TransactionsTable() {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
@@ -33,9 +33,18 @@ export function TransactionsTable() {
           {(transactions.map(transaction => (
             <tr key={transaction.id}>
               <td>{transaction.title}</td>
-              <td className={transaction.type}>{transaction.amount}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL'
+                }).format(transaction.amount)}
+              </td>
               <td>{transaction.category}</td>
-              <td>{transaction.createAt}</td>
+              <td>
+                {new Intl.DateTimeFormat('pt-BR').format(
+                  new Date(transaction.createdAt)
+                )} 
+              </td>
             </tr>
           )))}
       
