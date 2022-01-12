95495728cd201f5e09c7d865c61cb333dee4ee9d
Elan Fraga <elanfraga@gmail.com>
2022-01-11 22:36:43 -0300

    Listando posts em tela

--- 03-ignews/src/pages/posts/index.tsx
+++ 03-ignews/src/pages/posts/index.tsx
@@ -1,12 +1,24 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
 import { getPrismicClient } from '../../services/prismic';
 
 import styles from './styles.module.scss';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostProps) {
   return (
     <>
       <Head>
@@ -15,21 +27,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>11 janeiro de 2022</time>
-            <strong>Visões inovadoras do código livre e do open source</strong>
-            <p>Iniciativas de código aberto e softwares livres foram responsáveis em moldar o principal meio de comunicação moderno</p>
-          </a>
-          <a href="#">
-            <time>11 janeiro de 2022</time>
-            <strong>Visões inovadoras do código livre e do open source</strong>
-            <p>Iniciativas de código aberto e softwares livres foram responsáveis em moldar o principal meio de comunicação moderno</p>
-          </a>
-          <a href="#">
-            <time>11 janeiro de 2022</time>
-            <strong>Visões inovadoras do código livre e do open source</strong>
-            <p>Iniciativas de código aberto e softwares livres foram responsáveis em moldar o principal meio de comunicação moderno</p>
-          </a>
+          {posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          )) }          
         </div>
       </main>
     </>
@@ -46,10 +50,25 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100,
   })
 
-  console.log(JSON.stringify(response, null, 2));
+  // console.log(JSON.stringify(response, null, 2));
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type ==='paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    };
+  });
 
   return {
-    props: {}
+    props: {
+      posts
+    }
   }
 
 }
