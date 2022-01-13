0f2fea6ecaed0a5da304c7eba2ba690fdd7a5eac
Elan Fraga <elanfraga@gmail.com>
2022-01-12 21:49:41 -0300

    Gerando previews estáticos

--- 03-ignews/src/pages/posts/preview/[slug].tsx
+++ 03-ignews/src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next"
+import { GetStaticPaths, GetStaticProps } from "next"
 import { useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
@@ -57,10 +57,10 @@ export default function PostPreview({ post }: PostPreviewProps) {
     </>  )
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [],
-    fallback: 'blocking'
+    fallback: 'blocking',
   }
 }
 
@@ -89,6 +89,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     props: {
       post,
     },
+    revalidate: 60 * 30, // 30 minutes
   };
 
 }
